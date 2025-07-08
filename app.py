4882bead02ac97a9fd52a85a6cce8aea3b25eccb
Douglas Brunal <frankithy@gmail.com>
2025-07-08 11:44:18 -0500

    Update app.py
    updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="¡Hola desde el backend Flask with DockerPipeline!")
+    return jsonify(message="¡Hola desde el backend hecho en Python")
 
 @app.route('/saludo/<nombre>')
 def saludo(nombre):
